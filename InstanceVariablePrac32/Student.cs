912abfe1bf2f62b36dbd2f83e20b920f0c1f1397
Sohee Yun <s2128@e-mirim.hs.kr>
2023-05-09 12:09:01 +0900

    list에 들어있는 인스턴스를 삭제할 때 주의 점

--- InstanceVariablePrac32/Student.cs
+++ InstanceVariablePrac32/Student.cs
@@ -3,5 +3,11 @@
     internal class Student
     {
         internal string name;
+        internal int grade;
+
+        public override string ToString()
+        {
+            return grade + "학년 " + name;
+        }
     }
 }
